89ad6a78c5bf888b3d9cfd717f14c32aa8ccaa0a
M-V-SAKETH <m.saketh@tachyonsecurity.eu>
2025-08-18 21:10:53 +0530

    corrected import statement in custom piechart

--- frontend/expense-tracker/src/components/charts/CustomPieChart.jsx
+++ frontend/expense-tracker/src/components/charts/CustomPieChart.jsx
@@ -7,7 +7,7 @@ import {
   ResponsiveContainer,
   Legend,
 } from "recharts";
-import CustomTooltip from "./CustomTooltip";
+import CustomTooltip from "./CustomTooltip.jsx";
 
 const CustomPieChart = ({
   data,
