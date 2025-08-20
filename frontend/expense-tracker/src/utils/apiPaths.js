596a57b1373c988fdbb0594d80f4477f0aa0ca8f
M-V-SAKETH <m.saketh@tachyonsecurity.eu>
2025-08-20 16:08:13 +0530

    updated backend url

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-export const BASE_URL = "https://expense-tracker-zgt2.onrender.com"
+export const BASE_URL = "https://expense-tracker-backend1-tqtc.onrender.com"
 
 export const API_PATHS = {
   Auth: {
